1e8705a5f52c54df76fd221270316271371b3a23
mvdhnm <mvdhnm@umsystem.edu>
2025-04-05 16:46:02 -0500

    added tts, testing functionality

--- server/api.py
+++ server/api.py
@@ -5,6 +5,7 @@ import os
 import json
 from gemini_generation import gemini_generation
 from deep_translator import GoogleTranslator
+from google.cloud import texttospeech
 
 allergies = []
 data = {
@@ -99,6 +100,42 @@ async def upload_image(file: UploadFile = File(...)):
 # text to speech translation stuff
 @app.post("/translate/{language}")
 def text_to_speech(language: str):
-    text = f"Does this {last_food_name} contain any of these: {', '.join(last_maybe_allergies)}?"
+    # translation to specified language
+    text = f"Does this {last_food_name} contain any of these: {" ,".join(last_maybe_allergies)}."
+    supported_langs = GoogleTranslator().get_supported_languages(True)
     translation = GoogleTranslator(source='en', target=language).translate(text)
-    return translation
+
+    # text to speech
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=translation)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=supported_langs[language], ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    # output_path = "output.mp3"
+    # # The response's audio_content is binary.
+    # with open(output_path, "wb") as out:
+    #     # Write the response to the output file.
+    #     out.write(response.audio_content)
+    #     print('Audio content written to file "output.mp3"')
+    return translation, response
+
+
+# @app.get("/image/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None ):
+#     return {"item_id": item_id, "q": q}
